6c1f8ab328a1ef18ee6f5368d3b08eb461f81437
Julianne Kuettner <julianne.kuettner@thenobleacademy.org>
2018-08-14 19:07:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get  "/name" do
+    get '/name' do 
     "My name is Julianne"
-    
+  end
+
+  get '/hometown' do 
+    "My hometown is Davao City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Power Up"
 end
